2f4a1585284e8a6b5396fcdbb78e2e324bb09441
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-05 16:43:29 +0800

    定义实现最短匹配的正则表达式

---
+++ chapter2/c2.7.py
@@ -0,0 +1,12 @@
+# 定义实现最短匹配的正则表达式
+import re
+
+str_pat = re.compile(r'\"(.*)\"')
+text1 = 'Computer says "no."'
+str_pat.findall(text1)
+
+text2 = 'Computer says "no." Phone says "yes."'
+print(str_pat.findall(text2))
+
+str_pat = re.compile(r'\"(.*?)\"')
+print(str_pat.findall(text2))
