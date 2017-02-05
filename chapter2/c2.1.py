7705d830ae09f9f3df42cc68f0077d5322f3a8bd
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-05 11:14:18 +0800

    针对任意多的分隔符拆分字符串

--- chapter2/c2.1.py
+++ chapter2/c2.1.py
@@ -1,1 +1,14 @@
-import math
+# 针对任意多的分隔符拆分字符串
+# split()只能处理比较简单的情况.
+line = 'asdf fjdk; afed, fjek,asdf,    too'
+
+import re
+
+print(re.split(r'[;,\s]\s*', line))
+
+fields = re.split(r'(;|,|\s)\s*', line)
+print(fields)
+values = fields[::2]
+delimiters = fields[1::2] + ['']
+print(values)
+print(delimiters)
