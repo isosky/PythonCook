69c90bf5155d6e91a4d4f5676afd1525a010827a
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-15 21:18:07 +0800

    处理大型数组的计算

---
+++ chapter3/c2.8.py
@@ -0,0 +1,14 @@
+# 处理大型数组的计算
+x = [1, 2, 3, 4]
+y = [5, 6, 7, 8]
+print(x * 2)
+
+import numpy as np
+
+ax = np.array([1, 2, 3, 4])
+ay = np.array([5, 6, 7, 8])
+
+print(ax * 2)
+print(ax + 10)
+print(ax + ay)
+
