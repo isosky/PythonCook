457f76a02d1d2637eb8545536d967b3b85e5c92b
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-15 21:55:59 +0800

    随即选择

---
+++ chapter3/c3.11.py
@@ -0,0 +1,18 @@
+# 随机选择
+import random
+
+values = [1, 2, 3, 4, 5, 6]
+print(random.choice(values))
+print(random.choice(values))
+print(random.choice(values))
+print(random.choice(values))
+print(random.choice(values))
+print(random.choice(values))
+
+print(random.sample(values, 2))
+print(random.sample(values, 2))
+print(random.sample(values, 2))
+print(random.sample(values, 2))
+print(random.sample(values, 2))
+
+print(random.shuffle(values))
