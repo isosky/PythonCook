cd2ab488aee33e814517df541c041b409a21a448
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-16 23:41:48 +0800

    定义带有额外状态的生成器函数

---
+++ chapter4/c4.6.py
@@ -0,0 +1,17 @@
+# 定义带有额外状态的生成器函数
+from collections import deque
+
+
+class linehistory:
+    def __init__(self, lines, histlen=3):
+        self.lines = lines
+        self.history = deque(maxlen=histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines, 1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
+
