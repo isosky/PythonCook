c9a52738cc4b65f16e5d2ae505f4f684518872f8
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-20 23:24:06 +0800

    对已不存在的文件执行写入操作

--- chapter5/c5.5.py
+++ chapter5/c5.5.py
@@ -1,6 +1,9 @@
 # 对已不存在的文件执行写入操作
 import os
 
+with open('somefile', 'xt') as f:
+    f.write('hh')
+
 if not os.path.exists('somefile'):
     with open("somefile", 'wt') as f:
         f.write('Hello\n')
