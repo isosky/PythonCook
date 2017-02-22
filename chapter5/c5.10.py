e7e205e01433f1418a496c4eccee0894eea1411e
wangtr@asialantao.com <wangtr@asialantao.com>
2017-02-22 23:45:28 +0800

    对二进制文件做内存映射

---
+++ chapter5/c5.10.py
@@ -0,0 +1,17 @@
+# 对二进制文件做内存映射
+import os, mmap
+
+
+def memory_map(filename, access=mmap.ACCESS_WRITE):
+    size = os.path.getsize(filename)
+    fd = os.open(filename, os.O_RDWR)
+    return mmap.mmap(fd, size, access=access)
+
+
+# size = 100000
+# with open('data', 'wb') as f:
+#     f.seek(size - 1)
+#     f.write(b'\x00')
+
+m = memory_map('data')
+print(len(m))
